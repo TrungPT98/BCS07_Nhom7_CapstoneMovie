c64836c76541e3cb773f9d63d28e1d90f1140ad7
RayOlardo <phuclam1044@gmail.com>
2023-07-22 18:15:15 +0700

    add file assets

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex'>
+      <div className='w-1/2'>
+        
+      </div>
+      <div className='w-1/2'>
+      
+      </div>
+    </div>
+  )
+}
+
+export default Login
